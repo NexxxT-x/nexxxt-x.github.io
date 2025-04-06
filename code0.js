55bbc27ca1c879080100276a8c143f3ebc6d210a
NexxxT_x <50475950+NexxxT-x@users.noreply.github.com>
2025-04-06 23:32:07 +0200

    Update code0.js

--- code0.js
+++ code0.js
@@ -233,6 +233,12 @@ gdjs.MenuCode.userFunc0x9ee888(runtimeScene);
 
 };gdjs.MenuCode.userFunc0x1382388 = function GDJSInlineCode(runtimeScene) {
 "use strict";
+    Pi.init({ version: "2.0" }).then(() => {
+  Pi.authenticate(['username', 'payments'], { instantPayment: true }).then(authResult => {
+    gdjs.getGame().getVariables().get("PI_ACCESS_TOKEN").setString(authResult.accessToken);
+    // ... other setup
+  });
+});
 // Authenticate the user, and get permission to request payments from them:
 const scopes = ['payments'];
 

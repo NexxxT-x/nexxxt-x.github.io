0dee42efd2e84fadc5460fdd6094480ca53c6d1f
NexxxT_x <50475950+NexxxT-x@users.noreply.github.com>
2025-04-06 23:18:58 +0200

    Update code0.js

--- code0.js
+++ code0.js
@@ -199,9 +199,9 @@ Pi.createPayment({
   // Amount of π to be paid:
   amount: 3.14,
   // An explanation of the payment - will be shown to the user:
-  memo: "...", // e.g: "Digital kitten #1234",
+  memo: "xyz", // e.g: "Digital kitten #1234",
   // An arbitrary developer-provided metadata object - for your own usage:
-  metadata: { /* ... */ }, // e.g: { kittenId: 1234 }
+  metadata: { NexxxTId: 1111 }, // e.g: { kittenId: 1234 }
 }, {
   // Callbacks you need to implement - read more about those in the detailed docs linked below:
   onReadyForServerApproval: function(paymentId) { /* ... */ },
